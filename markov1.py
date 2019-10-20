cb13d2d71f20a10ebcaf00709bc5434e54b6fbc0
HugoT411ys <htmo@ic.ufal.br>
2019-10-20 11:58:37 -0300

    cadeia de markov questão 1 implementada

--- markov1.py
+++ markov1.py
@@ -9,20 +9,25 @@ class MarkovChain:
         self.prob = trans_prob
         self.states = list(trans_prob.keys())
         self.freq = {s: 0 for s in self.states}
+        self.num_iter = None
 
     def step_forward(self, curr_state):
         return numpy.random.choice(self.states, p=[self.prob[curr_state][next_state] for next_state in self.states])
 
     def frequencies(self):
-        return [(s, float(self.freq[s] / 1000)) for s in self.freq]
+        if self.num_iter is not None:
+            return [(s, float(self.freq[s] / self.num_iter)) for s in self.freq]
+        return 'You must first simulate the chain. Use <markov>.run()'
 
     def run(self, init_state, num_iter):
+        self.num_iter = num_iter
         for _ in range(num_iter):
             next_state = self.step_forward(curr_state=init_state)
             self.freq[next_state] = self.freq[next_state] + 1
             init_state = next_state
 
     def reset(self):
+        self.num_iter = None
         self.freq = {s: 0 for s in self.states}
 
 
